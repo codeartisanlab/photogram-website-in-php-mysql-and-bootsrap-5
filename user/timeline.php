e1f15f3bb74c04b3b83b5a6bd85c3abdf827bf65
codeartisanlab <codeartisanlab2607@gmail.com>
2024-09-30 16:56:10 +0530

    follower-following system added

--- user/timeline.php
+++ user/timeline.php
@@ -1,6 +1,23 @@
         <?php include('common/header.php'); ?>
 
         <?php
+            $myId=$_SESSION['user_id'];
+            $affectedRows=0;
+            if(isset($_POST['user_id'])){
+                $userId=$_POST['user_id'];
+                $action=$_POST['action'];
+                if($action=='follow'){
+                    $query="INSERT INTO followers (follower_id,following_id) VALUES ('$myId','$userId')";
+                    $result=mysqli_query($mysqli,$query);
+                    $affectedRows=mysqli_affected_rows($mysqli);
+                }else{
+                    $query="DELETE FROM followers WHERE follower_id='$myId' AND following_id='$userId'";
+                    $result=mysqli_query($mysqli,$query);
+                    $affectedRows=mysqli_affected_rows($mysqli);
+                }
+                
+            }
+
             $myInterest=$_SESSION['my_interest'];
             $myUserId=$_SESSION['user_id'];
             $query="SELECT * FROM users WHERE interested_in='$myInterest' AND user_id!='$myUserId'";
@@ -21,6 +38,14 @@
                         <?php
                             if($users!=''){
                                 foreach($users as $user){
+                                    $exist=false;
+                                    $userId=$user['user_id'];
+                                    $query="SELECT * FROM followers WHERE follower_id='$myId' AND following_id='$userId'";
+                                    $result=mysqli_query($mysqli,$query);
+                                    $numRows=mysqli_num_rows($result);
+                                    if($numRows>0){
+                                        $exist=true;
+                                    }
                                     ?>
                                     <div class="col-sm-4 col-12 mb-4">
                                         <div class="card">
@@ -30,7 +55,21 @@
                                                 <span class="badge bg-info text-dark mt-1"><?php echo $user['interested_in']; ?></span>
                                             </div>
                                             <div class="card-footer">
-                                                <button class="btn btn-dark bg-gradient btn-sm"><i class="fa fa-plus"></i> Follow Me</button>
+                                                <?php
+                                                if($exist==false){
+                                                ?>
+                                                <form method="post" action="">
+                                                    <input type="hidden" value="<?php echo $user['user_id']; ?>" name="user_id" />
+                                                    <input type="hidden" name="action" value="follow" />
+                                                    <button type="submit" class="btn btn-dark bg-gradient btn-sm"><i class="fa fa-plus"></i> Follow Me</button>
+                                                </form>
+                                                <?php }else{ ?>
+                                                    <form method="post" action="">
+                                                        <input type="hidden" value="<?php echo $user['user_id']; ?>" name="user_id" />
+                                                        <input type="hidden" name="action" value="unfollow" />
+                                                        <button type="submit" class="btn btn-success bg-gradient btn-sm"><i class="fa fa-check-circle"></i> Unfollow</button>
+                                                    </form>
+                                                <?php } ?> 
                                             </div>
                                         </div>
                                     </div>
